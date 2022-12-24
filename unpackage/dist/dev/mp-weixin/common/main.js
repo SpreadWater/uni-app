2a379b321d398acf9d1ba4f7a565c092fa6ab93d
SpreadWater <875737147@qq.com>
2022-12-24 13:46:40 +0800

    完成了 home 首页的开发

--- unpackage/dist/dev/mp-weixin/common/main.js
+++ unpackage/dist/dev/mp-weixin/common/main.js
@@ -8,22 +8,48 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-/* WEBPACK VAR INJECTION */(function(createApp) {
+/* WEBPACK VAR INJECTION */(function(uni, createApp) {
 
 var _interopRequireDefault = __webpack_require__(/*! @babel/runtime/helpers/interopRequireDefault */ 3);
 var _defineProperty2 = _interopRequireDefault(__webpack_require__(/*! @babel/runtime/helpers/defineProperty */ 10));
 __webpack_require__(/*! uni-pages */ 25);
 var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 24));
 var _App = _interopRequireDefault(__webpack_require__(/*! ./App */ 26));
+var _requestMiniprogram = __webpack_require__(/*! @escook/request-miniprogram */ 32);
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { (0, _defineProperty2.default)(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
 // @ts-ignore
 wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;
+uni.$http = _requestMiniprogram.$http;
+// 配置请求根路径
+_requestMiniprogram.$http.baseUrl = 'https://api-ugo-web.itheima.net';
+// $http.baseUrl = 'https://www.uinav.com'
+
+// 请求开始之前做一些事情
+_requestMiniprogram.$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...'
+  });
+};
+// 请求完成之后做一些事情
+_requestMiniprogram.$http.afterRequest = function () {
+  uni.hideLoading();
+};
+// 封装的展示消息提示的方法
+uni.$showMsg = function () {
+  var title = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '数据加载失败！';
+  var duration = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1500;
+  uni.showToast({
+    title: title,
+    duration: duration,
+    icon: 'none'
+  });
+};
 _vue.default.config.productionTip = false;
 _App.default.mpType = 'app';
 var app = new _vue.default(_objectSpread({}, _App.default));
 createApp(app).$mount();
-/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createApp"]))
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["default"], __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createApp"]))
 
 /***/ }),
 
